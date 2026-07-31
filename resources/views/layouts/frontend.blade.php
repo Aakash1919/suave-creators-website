2abdec78e4ba19612d0dd8750e3e70ab443734b4
Aakash1919 <programmer.aakash@gmail.com>
2026-07-31 15:17:05 +0530

    fixed header

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -47,7 +47,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
-    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+     @vite('resources/css/app.css')
     <script>
         tailwind.config = {
             theme: {
